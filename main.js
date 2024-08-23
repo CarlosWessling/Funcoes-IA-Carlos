6e0af7cf20062cb14ace7d99e474a7c6f8b73a52
Carlos Eduardo Wessling <107198663+CarlosWessling@users.noreply.github.com>
2024-08-23 09:39:41 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
-
-
-
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Pergunta?",
+        enunciado: "Gabriel",
         alternativas: [
             {
                 texto: " Texto/resposta",
